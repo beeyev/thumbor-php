ca214d7bf315739c7c7eee2f84d8e3ee3bec6404
Alexander Tebiev <alexander.tebiev@gmail.com>
2024-09-08 21:21:07 +0200

    changes

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,25 +1,19 @@
 <?php
 /**
- * This document has been generated with
- * https://mlocati.github.io/php-cs-fixer-configurator/#version:2.19.3|configurator
- *
  * @author Alexander Tebiev - https://github.com/beeyev
  */
 $config = new PhpCsFixer\Config();
 
 return $config
     ->setRiskyAllowed(true)
-    ->setUsingCache(false)
+    ->registerCustomFixers(new PhpCsFixerCustomFixers\Fixers())
     ->setRules([
-        '@PhpCsFixer:risky' => true,
         '@PhpCsFixer' => true,
-        '@PSR12' => true,
+        '@PER-CS2.0' => true,
+        '@PER-CS2.0:risky' => true,
         // Write conditions in Yoda style (`true`), non-Yoda style (`['equal' => false, 'identical' => false, 'less_and_greater' => false]`) or ignore those conditions (`null`) based on configuration.
         'yoda_style' => false,
-        // Visibility MUST be declared on all properties and methods; `abstract` and `final` MUST be declared before the visibility; `static` MUST be declared after the visibility.
-        'visibility_required' => ['elements' => ['method', 'property']],
-        // Concatenation should be spaced according configuration.
-        'concat_space' => ['spacing' => 'one'],
+
         // Sorts PHPDoc types.
         'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
         // Single-line comments and multi-line comments with only one line of actual content should use the `//` syntax.
@@ -30,6 +24,56 @@ return $config
         'phpdoc_summary' => false,
         // `@return void` and `@return null` annotations should be omitted from PHPDoc.
         'phpdoc_no_empty_return' => false,
+        // Forbid multi-line whitespace before the closing semicolon or move the semicolon to the new line for chained calls.
+        'multiline_whitespace_before_semicolons' => false,
+        // Comments must be surrounded by spaces.
+        PhpCsFixerCustomFixers\Fixer\CommentSurroundedBySpacesFixer::name() => true,
+        // Constructor's empty braces must be single line.
+        PhpCsFixerCustomFixers\Fixer\ConstructorEmptyBracesFixer::name() => true,
+        // There can be no imports from the global namespace.
+        PhpCsFixerCustomFixers\Fixer\NoImportFromGlobalNamespaceFixer::name() => true,
+        // Trailing comma in the list on the same line as the end of the block must be removed.
+        PhpCsFixerCustomFixers\Fixer\NoTrailingCommaInSinglelineFixer::name() => true,
+        // There must be no useless parentheses.
+        PhpCsFixerCustomFixers\Fixer\NoUselessParenthesisFixer::name() => true,
+        // The strlen or mb_strlen functions should not be compared against 0.
+        PhpCsFixerCustomFixers\Fixer\NoUselessStrlenFixer::name() => true,
+        // There must be no superfluous parameters in PHPDoc.
+        PhpCsFixerCustomFixers\Fixer\PhpdocNoSuperfluousParamFixer::name() => true,
+        // The @var annotations must be on a single line if they are the only content.
+        PhpCsFixerCustomFixers\Fixer\PhpdocSingleLineVarFixer::name() => true,
+        // PHPDoc types must be trimmed.
+        PhpCsFixerCustomFixers\Fixer\PhpdocTypesTrimFixer::name() => true,
+        // Statements not preceded by a line break must be preceded by a single space.
+        PhpCsFixerCustomFixers\Fixer\SingleSpaceBeforeStatementFixer::name() => true,
+        // In PHPDoc, the class or interface element self should be preferred over the class name itself.
+        PhpCsFixerCustomFixers\Fixer\PhpdocSelfAccessorFixer::name() => true,
+        // Empty function body must be abbreviated as {} and placed on the same line as the previous symbol, separated with a space.
+        PhpCsFixerCustomFixers\Fixer\EmptyFunctionBodyFixer::name() => true,
+        // PHPDoc types commas must not be preceded by a whitespace, and must be succeeded by a single whitespace.
+        PhpCsFixerCustomFixers\Fixer\PhpdocTypesCommaSpacesFixer::name() => true,
+        // Calls to `PHPUnit\Framework\TestCase` static methods must all be of the same type, either `$this->`, `self::` or `static::`.
+        'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
+        // Removes the leading part of fully qualified symbol references if a given symbol is imported or belongs to the current namespace.
+        'fully_qualified_strict_types' => false,
+        'php_unit_test_class_requires_covers' => false,
+        'php_unit_strict' => false,
+        'string_implicit_backslashes' => false,
+        'ordered_class_elements' => [
+            'order' => [
+                'use_trait',
+                'constant',
+                'property',
+                'construct',
+                'method:__invoke',
+                'public',
+                'protected',
+                'private',
+                'magic',
+            ],
+            'sort_algorithm' => 'none',
+            'case_sensitive' => false,
+        ],
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
@@ -42,6 +86,5 @@ return $config
             ])
             ->append([
                 __DIR__ . '/.php-cs-fixer.dist.php',
-            ])
-    )
-;
+            ]),
+    );
--- Makefile
+++ Makefile
@@ -8,14 +8,11 @@ help: ## Show this help
 	@printf "\033[33m%s:\033[0m\n" 'Available commands'
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "  \033[32m%-14s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
-stan: ## Execute PHPStan (PHP 7.2 Required)
-	php phpstan-v1.10.phar analyse --configuration=phpstan.neon.dist --memory-limit=1G
+stan: ## Execute PHPStan
+	phpstan analyse --configuration=phpstan.neon.dist -v
 
-cs: ## Execute PHP CS Fixer (PHP 7.2 Required)
-	php php-cs-fixer-v2.19.phar fix --diff --verbose --using-cache=yes --cache-file=.php-cs-fixer.cache
+cs: ## Execute PHP CS Fixer
+	php-cs-fixer fix --diff -vv
 
 test: ## Execute PHPUnit
 	phpunit --testdox
-
-rector: ## Execute Rector PHP
-	rector process
